bb088968fcf2f71cda683258cd3c1447b4884200
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-01-01 01:07:22 +0700

    The Open Graph protocol  Changes to be committed: 	modified:   resources/views/includes/head.blade.php

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,3 +1,8 @@
+    /*
+     * TODO :      
+     * add meta description
+     * add The Open Graph protocol
+     */
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
