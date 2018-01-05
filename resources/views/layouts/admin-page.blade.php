55186c97f4f81524e50efc6ec39e9a1bc6f6e10b
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-01-05 15:14:38 +0700

    fix some blade  Changes to be committed: 	modified:   public/css/app.css 	modified:   resources/views/layouts/admin-page.blade.php 	modified:   resources/views/layouts/auth.blade.php

--- resources/views/layouts/admin-page.blade.php
+++ resources/views/layouts/admin-page.blade.php
@@ -1,21 +1,19 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        @include('includes.head')
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <link rel="icon" href="{{ asset('favicon.ico') }}">
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('fonts/font-awesome/css/font-awesome.min.css') }}">
     </head>
     <body class="admin-page">
         <div class="admin-wrapper">
-<<<<<<< HEAD:resources/views/layouts/admin.blade.php
-            @include('includes.admin.sidebar')
-            @include('includes.admin.navbar')
-
-            <div class="admin-content-wrap">
-                <main class="admin-main">
-                    @yield('content')
-                </main>
-
-                @include('includes.admin.footer')
-=======
             @include('includes.admin-page.sidebar')
             @include('includes.admin-page.navbar')
 
@@ -23,7 +21,6 @@
                 <div class="admin-main">
                     @yield('content')
                 </div>
->>>>>>> e8ba71dcc01b70ba51cb3acc9663662f06c8c522:resources/views/layouts/admin-page.blade.php
             </div>
         </div>
 
