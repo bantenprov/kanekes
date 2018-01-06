76014b822c11563a32b57d3082790d943f6e3b0a
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-01-06 19:26:34 +0700

    rebase from bantenprov  Changes to be committed: 	modified:   README.md 	modified:   composer.json 	modified:   public/css/app.css 	modified:   public/mix-manifest.json 	modified:   resources/assets/sass/_admin.scss 	new file:   resources/assets/sass/app.scss 	modified:   resources/views/layouts/3-column.blade.php 	new file:   resources/views/layouts/admin.blade.php 	modified:   resources/views/layouts/auth.blade.php 	modified:   resources/views/pages/landing-page/section-2.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,6 @@ Route::get('/', function () {
     return view('pages.landing-page.index');
 });
 
-<<<<<<< HEAD
-Route::get('admin', function () {
-    return view('admin');
-});
-
-Route::get('login', function () {
-    return view('auth.login');
-});
-
-Route::get('register', function () {
-    return view('auth.register');
-=======
 Route::get('/admin', function () {
     return view('pages.admin-page.home.index');
 });
@@ -26,7 +14,6 @@ Route::get('/register', function () {
 
 Route::get('/login', function () {
     return view('pages.auth-page.login');
->>>>>>> e8ba71dcc01b70ba51cb3acc9663662f06c8c522
 });
 
 Route::get('/3-column-example', function () {
